b47de8264b7358bdde6514aa1bbf98d9a330a698
AceTerrific <128358149+AceTerrific@users.noreply.github.com>
2023-06-26 15:15:48 +0800

    add function on closing thumbnail

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const heartBtn = document.querySelectorAll(".heartBtn");
+const closeBtn = document.querySelectorAll(".close-button");
+const readBtn = document.querySelectorAll(".read-icon");
 
 heartBtn.forEach((element) => {
   element.addEventListener("click", () => {
-    element.classList.toggle("blue-heart");
+    element.classList.toggle("blue");
     if (element.firstElementChild.getAttribute("data-prefix") == "far") {
       element.firstElementChild.setAttribute("data-prefix", "fas");
     } else element.firstElementChild.setAttribute("data-prefix", "far");
+  });
+});
+
+closeBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.closest(".box").remove();
+  });
+});
+
+readBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("blue");
+    console.log(element.firstElementChild.getAttribute("class"));
+
+    if (element.firstElementChild.getAttribute("data-prefix") == "far") {
+      element.firstElementChild.setAttribute("data-prefix", "fas");
+      element.firstElementChild.setAttribute("data-icon", "circle-check");
+    } else {
+      element.firstElementChild.setAttribute("data-prefix", "far");
+      element.firstElementChild.setAttribute("data-icon", "circle");
+    }
   });
 });
